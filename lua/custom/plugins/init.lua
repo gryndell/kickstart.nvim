f06a2cc304b472536a941bfcf313814478152762
gryndell <alfred.pengelly@gmail.com>
2024-02-05 09:02:29 +1000

    Replaced tabs with 2 spaces each.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,103 +1,103 @@
 return {
-	{
-	    'windwp/nvim-autopairs',	-- pairs of parentheses etc.
-	    event = "InsertEnter",
-	    opts = {} -- this is equalent to setup({}) function
-	},
-	'kshenoy/vim-signature',      -- show marks in gutter
+  {
+      'windwp/nvim-autopairs',  -- pairs of parentheses etc.
+      event = "InsertEnter",
+      opts = {} -- this is equalent to setup({}) function
+  },
+  'kshenoy/vim-signature',      -- show marks in gutter
   'dhruvasagar/vim-table-mode', -- table mode
-	'gabrielelana/vim-markdown',  -- better markdown syntax highlighting
-	'Exafunction/codeium.vim',    -- AI assistant
-	config = function ()
-	-- Change '<C-g>' here to any keycode you like.
-	vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end,
-				{ expr = true, silent = true })
-	vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end,
-				{ expr = true, silent = true })
-	end,
-	{
-		'akinsho/bufferline.nvim',	-- show buffers in tabline
-		version = "*",
-		dependencies = 'nvim-tree/nvim-web-devicons',
-		highlights = {
-			fill = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			background = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			buffer_visible = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			close_button = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			close_button_visible = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			tab_selected = {
-				fg = { attribute = "fg", highlight = "Normal" },
-				bg = { attribute = "bg", highlight = "Normal" },
-			},
-			tab = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			tab_close = {
-				-- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
-				fg = { attribute = "fg", highlight = "TabLineSel" },
-				bg = { attribute = "bg", highlight = "Normal" },
-			},
-			duplicate_selected = {
-				fg = { attribute = "fg", highlight = "TabLineSel" },
-				bg = { attribute = "bg", highlight = "TabLineSel" },
-				italic = true,
-			},
-			duplicate_visible = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-				italic = true,
-			},
-			duplicate = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-				italic = true,
-			},
+  'gabrielelana/vim-markdown',  -- better markdown syntax highlighting
+  'Exafunction/codeium.vim',    -- AI assistant
+  config = function ()
+  -- Change '<C-g>' here to any keycode you like.
+  vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end,
+        { expr = true, silent = true })
+  vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end,
+        { expr = true, silent = true })
+  end,
+  {
+    'akinsho/bufferline.nvim',  -- show buffers in tabline
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    highlights = {
+      fill = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      background = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      buffer_visible = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      close_button = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      close_button_visible = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      tab_selected = {
+        fg = { attribute = "fg", highlight = "Normal" },
+        bg = { attribute = "bg", highlight = "Normal" },
+      },
+      tab = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      tab_close = {
+        -- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
+        fg = { attribute = "fg", highlight = "TabLineSel" },
+        bg = { attribute = "bg", highlight = "Normal" },
+      },
+      duplicate_selected = {
+        fg = { attribute = "fg", highlight = "TabLineSel" },
+        bg = { attribute = "bg", highlight = "TabLineSel" },
+        italic = true,
+      },
+      duplicate_visible = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+        italic = true,
+      },
+      duplicate = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+        italic = true,
+      },
 
-			modified = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			modified_selected = {
-				fg = { attribute = "fg", highlight = "Normal" },
-				bg = { attribute = "bg", highlight = "Normal" },
-			},
-			modified_visible = {
-				fg = { attribute = "fg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
+      modified = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      modified_selected = {
+        fg = { attribute = "fg", highlight = "Normal" },
+        bg = { attribute = "bg", highlight = "Normal" },
+      },
+      modified_visible = {
+        fg = { attribute = "fg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
 
-			separator = {
-				fg = { attribute = "bg", highlight = "TabLine" },
-				bg = { attribute = "bg", highlight = "TabLine" },
-			},
-			separator_selected = {
-				fg = { attribute = "bg", highlight = "Normal" },
-				bg = { attribute = "bg", highlight = "Normal" },
-			},
-			-- separator_visible = {
-			--   fg = {attribute='bg',highlight='TabLine'},
-			--   bg = {attribute='bg',highlight='TabLine'}
-			--   },
-			indicator_selected = {
-				fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-				bg = { attribute = "bg", highlight = "Normal" },
-			},
-		},
-	},
+      separator = {
+        fg = { attribute = "bg", highlight = "TabLine" },
+        bg = { attribute = "bg", highlight = "TabLine" },
+      },
+      separator_selected = {
+        fg = { attribute = "bg", highlight = "Normal" },
+        bg = { attribute = "bg", highlight = "Normal" },
+      },
+      -- separator_visible = {
+      --   fg = {attribute='bg',highlight='TabLine'},
+      --   bg = {attribute='bg',highlight='TabLine'}
+      --   },
+      indicator_selected = {
+        fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+        bg = { attribute = "bg", highlight = "Normal" },
+      },
+    },
+  },
 }

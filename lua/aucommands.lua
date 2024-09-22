812416fa66a783902ba66a3026294501fabeb8f1
gryndell <alfred.pengelly@gmail.com>
2024-09-22 14:28:04 +1000

    Changed ts, sw, and sts to 8 for C, Go, and Rust

--- lua/aucommands.lua
+++ lua/aucommands.lua
@@ -88,7 +88,7 @@ augroup file_types
   autocmd BufRead,BufNewFile *.ms,*.me,*.mom setlocal ft=nroff
   autocmd BufRead,BufNewFile *.c,*.py,*.js,*.java,*.vim,*rc
     \ run :IndentLinesEnable<cr>
-  autocmd BufRead,BufNewFile *.c setlocal sw=4 ts=4 sts=4
+  autocmd BufRead,BufNewFile *.c,*.go,*.rs setlocal sw=8 ts=8 sts=8
   " autocmd BufWritePost       *.c,*.py,*.js,*.java silent! !ctags -R &
   autocmd BufRead,BufNewFile Makefile
     \ setlocal sw=4 ts=4 sts=4 noet

dc9f8a685b415f91270045e50a6fb2a8f762c86c
gryndell <alfred.pengelly@gmail.com>
2024-09-25 06:10:18 +1000

    Config for Monokai-Nightasty - No italic comments

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,15 +1,5 @@
 --[[ colorscheme.lua ]]
-
--- require('onedark').setup {
---     -- Set a style preset. 'dark' is default.
---     style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
---     no_italic = false,
---     no_bold = false,
---     no_underline = false,
--- }
--- require('onedark').load()
 vim.opt.background = 'dark'
 vim.cmd.colorscheme("monokai-nightasty")
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -152,30 +152,82 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require('onedark').setup {
-        -- Set a style preset. 'dark' is default.
-        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-        no_italic = false,
-        no_bold = false,
-        no_underline = false,
-      }
-      require('onedark').load()
-      vim.cmd [[ highlight Normal guibg=NONE ]]
-      vim.cmd [[ highlight EndOfBuffer guibg=NONE ]]
-    end,
-  },
+-- {
+--   -- Theme inspired by Atom
+--   'navarasu/onedark.nvim',
+--   priority = 1000,
+--   lazy = false,
+--   config = function()
+--     require('onedark').setup {
+--       -- Set a style preset. 'dark' is default.
+--       style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+--       no_italic = false,
+--       no_bold = false,
+--       no_underline = false,
+--     }
+--     require('onedark').load()
+--     vim.cmd [[ highlight Normal guibg=NONE ]]
+--     vim.cmd [[ highlight EndOfBuffer guibg=NONE ]]
+--   end,
+-- },
 
   {
     -- Monokai-Nightasty
     "polirritmico/monokai-nightasty.nvim",
     lazy = false,
     priority = 1000,
+    keys = {
+      { "<leader>tt", "<Cmd>MonokaiToggleLight<CR>", desc = "Monokai-Nightasty: Toggle dark/light theme." },
+    },
+    ---@type monokai.UserConfig
+    opts = {
+      dark_style_background = "transparent", -- default, dark, transparent, #color
+      light_style_background = "default", -- default, dark, transparent, #color
+      color_headers = true, -- Enable header colors for each header level (h1, h2, etc.)
+      lualine_bold = true, -- Lualine a and z sections font width
+      lualine_style = "dark", -- "dark", "light" or "default" (Follows dark/light style)
+      markdown_header_marks = true, -- Add headers marks highlights (the `#` character) to Treesitter highlight query
+      -- Style to be applied to different syntax groups. See `:help nvim_set_hl`
+      hl_styles = {
+        comments = { italic = false },
+        keywords = { italic = true },
+      },
+
+      -- This also could be a table like this: `terminal_colors = { Normal = { fg = "#e6e6e6" } }`
+      terminal_colors = function(colors)
+        return { fg = colors.fg_dark }
+      end,
+
+      --- You can override specific color/highlights. Theme color values
+      --- in `extras/palettes`. Also could be any hex RGB color you like.
+      on_colors = function(colors)
+        -- Custom color only for light theme
+        local current_is_light = vim.o.background == "light"
+        colors.comment = current_is_light and "#2d7e79" or colors.grey
+
+        -- Custom color only for dark theme
+        colors.border = not current_is_light and colors.magenta or colors.border
+
+        -- Custom lualine normal color
+        colors.lualine.normal_bg = current_is_light and "#7ebd00" or colors.green
+      end,
+
+      on_highlights = function(highlights, colors)
+        -- You could add styles like bold, underline, italic
+        highlights.TelescopeSelection = { bold = true }
+        highlights.TelescopeBorder = { fg = colors.grey }
+        highlights["@lsp.type.property.lua"] = { fg = colors.fg }
+      end,
+    },
+    config = function(_, opts)
+      -- Highlight line at the cursor position
+      vim.opt.cursorline = true
+
+      -- Default to dark theme
+      vim.o.background = "dark"  -- dark | light
+
+      require("monokai-nightasty").load(opts)
+    end,
   },
 
   {

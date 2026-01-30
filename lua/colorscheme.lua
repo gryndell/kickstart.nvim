ee3612816174c1805feeba6ef0f2459e8b548c35
gryndell <alfred.pengelly@gmail.com>
2026-01-31 08:40:03 +1000

    Switched to colorscheme elflord.

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,5 @@
 --[[ colorscheme.lua ]]
 vim.opt.background = 'dark'
-vim.cmd.colorscheme("monokai-nightasty")
+vim.cmd.colorscheme("elflord")
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })

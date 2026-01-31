a63ed3abe4016b44565587a96f8f68dd2083f7bb
gryndell <alfred.pengelly@gmail.com>
2026-02-01 08:44:51 +1000

    Grey line numbers and ColorColumn.

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,3 +3,7 @@ vim.opt.background = 'dark'
 vim.cmd.colorscheme("elflord")
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'LineNr', { fg = 'yellow' })
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'grey' })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'grey' })
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = 'grey' })
